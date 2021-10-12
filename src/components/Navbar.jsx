96438e7bc2ecda62c1ae4c8c49aa73d0556563ae
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-12 03:55:36 -0400

    Navbar Implemented

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
+import "./Navbar.scss";
+import { Avatar } from "@material-ui/core";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__left">
+          <span className="navbar__logo">Webflix</span>
+          <div className="navbar__links">
+            <span>Home</span>
+            <span>TV Shows</span>
+            <span>Movies</span>
+            <span>New &amp; Popular</span>
+            <span>My List</span>
+          </div>
+        </div>
+        <div className="navbar__right">
+          <Search style={{ fontSize: "2.6rem", margin: "0 1rem" }} />
+          <span>Kids</span>
+          <Notifications style={{ fontSize: "2.6rem", margin: "0 1rem" }} />
+          <Avatar
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+            variant="rounded"
+            style={{ width: 32, height: 32, marginLeft: "1rem" }}
+          />
+          <ArrowDropDown style={{ fontSize: "2.5rem" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
