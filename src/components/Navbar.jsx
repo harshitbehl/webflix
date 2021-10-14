30995c38db78e6690350267d0b0498f71486d502
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-14 17:21:03 -0400

    Updated Navbar Links Style, Implemented Dropdown Menu & Added Transitions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
 import "./Navbar.scss";
 import { Avatar } from "@material-ui/core";
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // JS Window Scroll Method, whenever we scroll it runs the function
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+  };
+
+  console.log(isScrolled);
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar--scrolled" : "navbar"}>
       <div className="navbar__container">
         <div className="navbar__left">
           <span className="navbar__logo">Webflix</span>
@@ -18,15 +27,30 @@ function Navbar() {
           </div>
         </div>
         <div className="navbar__right">
-          <Search style={{ fontSize: "2.6rem", margin: "0 1rem" }} />
+          <Search
+            className="navbar__icon"
+            style={{ fontSize: "2.6rem", margin: "0 1rem" }}
+          />
           <span>Kids</span>
-          <Notifications style={{ fontSize: "2.6rem", margin: "0 1rem" }} />
+          <Notifications
+            className="navbar__icon"
+            style={{ fontSize: "2.6rem", margin: "0 1rem" }}
+          />
           <Avatar
             src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
             variant="rounded"
             style={{ width: 32, height: 32, marginLeft: "1rem" }}
           />
-          <ArrowDropDown style={{ fontSize: "2.5rem" }} />
+          <div className="navbar__dropdown">
+            <ArrowDropDown
+              className="navbar__icon navbar__arrowdropdown-icon"
+              style={{ fontSize: "2.5rem" }}
+            />
+            <div className="navbar__dropdown-options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,10 @@ function Home() {
   return (
     <div className="home">
       <Navbar />
+      <img
+        src="https://images.unsplash.com/photo-1569003339405-ea396a5a8a90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        alt=""
+      />
     </div>
   );
 }
